68d9c5fb7f4f5ac1b24252805c3393fca7eb976d
franciscoL0pez <franlopez.2001@hotmail.com>
2025-04-05 11:12:40 -0300

    Add test db connect and alembic for migrations

--- dbConfig/postgres.py
+++ dbConfig/postgres.py
@@ -2,22 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+from sqlalchemy.ext.declarative import declarative_base
 
-# Load environment variables from .env file
+
 load_dotenv()
-# Database connection URL
 DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@" \
     f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
-# Create a new SQLAlchemy engine instance
+
 
 engine = create_engine(DATABASE_URL)
-
-# Create a new session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
-# Create a new session
+
 def get_db():
+
     db = SessionLocal()
     try:
         yield db
--- tests/test_postgres_connect.py
+++ tests/test_postgres_connect.py
@@ -1,10 +1,19 @@
 import pytest
-from dbConfig.postgres import connect_to_db  # Asegúrate de que el import sea correcto
-from dbConfig.postgres import create_database  # Asegúrate de que el import sea correcto
-import sys
+from sqlalchemy.exc import OperationalError
+from dbConfig import postgres
 import os
+from dotenv import load_dotenv
+load_dotenv()
+import sys
+
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-
+def test_database_connection():
+    try:
+        
+        with postgres.engine.connect() as conn:
+            assert conn.closed == False
+    except OperationalError as e:
+        pytest.fail(f"Not able to connect to the database: {e}")
 
