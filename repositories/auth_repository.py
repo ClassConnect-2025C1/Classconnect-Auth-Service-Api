85f99bf9762de72b46ffe01dcef11220838818cf
Matias <matias32p@gmail.com>
2025-04-15 11:47:42 -0300

    feat: repository handle craete y get verification pin

--- repositories/auth_repository.py
+++ repositories/auth_repository.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from utils.security import hash_password
-from models.credential_models import Credential
+from models.credential_models import Credential, VerificationPin
+from datetime import datetime, timezone
 
 def get_user_by_email(db: Session, email: str):
     return db.query(Credential).filter(Credential.email == email).first()
@@ -11,3 +12,13 @@ def create_user(db: Session, email: str, password: str):
     db.commit()
     db.refresh(user)
     return user
+
+def create_verification_pin(db: Session, user_id: str, pin: str):
+    verification_pin = VerificationPin(user_id=user_id, pin=pin, created_at=datetime.now(timezone.utc) )
+    db.add(verification_pin)
+    db.commit()
+    db.refresh(verification_pin)
+    return verification_pin
+
+def get_verification_pin(db: Session, user_id: str):
+    return db.query(VerificationPin).filter(VerificationPin.user_id == user_id).first()
