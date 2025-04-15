7c8f0911d2b75f37dcf50baf6a77d4294ccb7013
Matias <matias32p@gmail.com>
2025-04-15 11:49:18 -0300

    test:  creation and get interaction with db

--- tests/unitTest/test_repositories.py
+++ tests/unitTest/test_repositories.py
@@ -1,11 +1,9 @@
 # tests/unitTest/test_auth_repository.py
 
 from unittest.mock import MagicMock
-from repositories.auth_repository import create_user, get_user_by_email
-from models.credential_models import Credential
-from routes import auth
-from controller import service_controller
-from services import auth_services
+from repositories.auth_repository import create_user, get_user_by_email, create_verification_pin, get_verification_pin
+from models.credential_models import Credential, VerificationPin
+from datetime import datetime, timezone
 
 def test_create_user_with_mock():
     mock_db = MagicMock()
@@ -70,3 +68,60 @@ def test_user_is_not_verified_when_created():
 
     assert isinstance(user, Credential)
     assert user.is_verified is False
+
+def test_create_verification_pin_returns_model():
+    mock_db = MagicMock()
+    mock_add = mock_db.add
+    mock_commit = mock_db.commit
+    mock_refresh = mock_db.refresh
+
+    user_id = "user@example.com"
+    pin = "123456"
+
+    result = create_verification_pin(mock_db, user_id, pin)
+
+    assert isinstance(result, VerificationPin)
+    assert result.user_id == user_id
+    assert result.pin == pin
+    assert isinstance(result.created_at, datetime)
+    assert result.created_at.tzinfo is not None
+
+def test_create_verification_pin_sets_recent_timestamp():
+    mock_db = MagicMock()
+    user_id = "13456"
+    pin = "123456"
+
+    before = datetime.now(timezone.utc)
+
+    result = create_verification_pin(mock_db, user_id, pin)
+
+    after = datetime.now(timezone.utc)
+
+    # Asegura que el tiempo de creación esté entre `before` y `after`
+    assert before <= result.created_at <= after
+
+def test_get_not_exist_pin_return_none():
+    mock_db = MagicMock()
+    user_id = "1234567890"
+
+    # Simula que no encuentra ningún usuario
+    mock_db.query.return_value.filter.return_value.first.return_value = None
+
+    result = get_verification_pin(mock_db, user_id)
+
+    assert result is None
+
+def test_get_veritication_pin_success():
+    mock_db = MagicMock()
+    user_id = "1234567890"
+    pin = "123456"
+
+    # Simula que encuentra el pin
+    mock_pin = VerificationPin(user_id=user_id, pin=pin, created_at=datetime.now(timezone.utc))
+    mock_db.query.return_value.filter.return_value.first.return_value = mock_pin
+
+    result = get_verification_pin(mock_db, user_id)
+
+    assert result is not None
+    assert result.user_id == user_id
+    assert result.pin == pin
